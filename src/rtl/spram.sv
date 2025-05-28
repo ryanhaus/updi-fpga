111634af84281be33c89ceb6f646919daa8707dd
Ryan Hausmann <ryanhausmann3@gmail.com>
2025-05-28 13:08:17 -0400

    Add spram module & testbench

---
+++ src/rtl/spram.sv
@@ -0,0 +1,50 @@
+typedef enum {
+	SPRAM_IDLE,
+	SPRAM_READ,
+	SPRAM_WRITE
+} spram_mode;
+
+// Single-port random-access memory
+module spram #(
+	parameter SIZE = 1,
+	parameter DATA_BITS = 8,
+	parameter ADDR_BITS = $clog2(SIZE),
+	parameter DELETE_ON_RESET = 0
+) (
+	input clk,
+	input rst,
+
+	input spram_mode mode,
+
+	input [ADDR_BITS-1 : 0] rd_addr,
+	input [ADDR_BITS-1 : 0] wr_addr,
+
+	input [DATA_BITS-1 : 0] data_wr,
+	output logic [DATA_BITS-1 : 0] data_rd
+);
+
+	logic [DATA_BITS-1 : 0] mem [SIZE];
+
+	integer i;
+
+	always @(posedge clk) begin
+		if (rst && DELETE_ON_RESET) begin
+			for (i = 0; i < SIZE; i = i + 1)
+				mem[i] = 'b0;
+		end
+		else begin
+			case (mode)
+				SPRAM_IDLE: begin end
+
+				SPRAM_READ: begin
+					data_rd = mem[rd_addr];
+				end
+
+				SPRAM_WRITE: begin
+					mem[wr_addr] = data_wr;
+				end
+			endcase
+		end
+	end
+
+endmodule
---
+++ src/test/tb_spram.sv
@@ -0,0 +1,41 @@
+// Testbench for spram module
+module tb_spram();
+
+	logic clk, rst;
+	spram_mode mode;
+	logic [dut.ADDR_BITS-1 : 0] rd_addr, wr_addr;
+	logic [dut.DATA_BITS-1 : 0] data_wr, data_rd;
+	spram #(.SIZE(32)) dut (clk, rst, mode, rd_addr, wr_addr, data_wr, data_rd);
+
+	integer i;
+
+	initial begin
+		$dumpfile("trace/tb_spram.vcd");
+		$dumpvars();
+
+		// test writes
+		mode = SPRAM_WRITE;
+
+		for (i = 0; i < 32; i = i + 1) begin
+			#10 clk = 'b0;
+			wr_addr = i[dut.ADDR_BITS-1 : 0];
+			data_wr = i[dut.DATA_BITS-1 : 0];
+
+			#10 clk = 'b1;
+		end
+
+		// test reads
+		#10 clk = 'b0;
+		mode = SPRAM_READ;
+
+		for (i = 0; i < 32; i = i + 1) begin
+			rd_addr = i[dut.ADDR_BITS-1 : 0];
+
+			#10 clk = 'b1;
+			#10 clk = 'b0;
+
+			if (data_rd != i[dut.DATA_BITS-1 : 0]) $error();
+		end
+	end
+
+endmodule
