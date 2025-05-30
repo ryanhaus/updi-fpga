1c4978eabcf20a307a79a1c4bc232d3d03cbe6c1
Ryan Hausmann <ryanhausmann3@gmail.com>
2025-05-30 11:56:50 -0400

    Add module for decoding program files

---
+++ src/rtl/program_decoder.sv
@@ -0,0 +1,119 @@
+// All the states for the decoder
+typedef enum {
+	PROG_DECODER_IDLE,
+	PROG_DECODER_READ_LENGTH,
+	PROG_DECODER_READ_ADDRESS,
+	PROG_DECODER_READ_TYPE,
+	PROG_DECODER_READ_DATA
+} program_decoder_state;
+
+// Program decoder module. Programs are stored similar to Intel hex files but
+// with certain metadata stripped away. See src/util/program_convert.py
+/* verilator lint_off WIDTHEXPAND */
+module program_decoder #(
+	parameter PROGRAM_SIZE = 1, // size of program in bytes
+	parameter PROG_ADDR_BITS = $clog2(PROGRAM_SIZE), // bits needed for program address
+	parameter DATA_BLOCK_MAX_SIZE = 64, // max number of data bytes per block
+	parameter DATA_BLOCK_ADDR_BITS = $clog2(DATA_BLOCK_MAX_SIZE) // bits for data in block
+) (
+	input clk,
+	input rst,
+
+	// program interface, assumes sync 8-bit ROM
+	output logic [PROG_ADDR_BITS-1 : 0] prog_addr,
+	input [7:0] prog_data,
+
+	// control signals
+	input start,
+	output logic ready,
+	output logic done,
+
+	// outputs
+	output logic [7:0] block_length,
+	output logic [15:0] block_address,
+	output logic [7:0] block_type,
+	output logic [7:0] block_data [DATA_BLOCK_MAX_SIZE]
+);
+
+	// program decoder state machine
+	program_decoder_state state;
+	logic [DATA_BLOCK_ADDR_BITS-1 : 0] counter;
+
+	always_ff @(posedge clk) begin
+		if (rst) begin
+			state = PROG_DECODER_IDLE;
+			counter = 'b0;
+
+			ready = 'b0;
+			done = 'b0;
+			prog_addr = 'b0;
+		end
+		else begin
+			case (state)
+				PROG_DECODER_IDLE: begin
+					// wait idle until start signal
+					ready = 'b1;
+					counter = 'b0;
+
+					if (start) begin
+						done = 'b0;
+						ready = 'b0;
+						state = PROG_DECODER_READ_LENGTH;
+					end
+				end
+
+				PROG_DECODER_READ_LENGTH: begin
+					// current value in ROM represents the block length
+					block_length = prog_data;
+
+					// increase addr, next state
+					prog_addr = prog_addr + 'b1;
+					state = PROG_DECODER_READ_ADDRESS;
+				end
+
+				PROG_DECODER_READ_ADDRESS: begin
+					// address is a 2-byte value, MSB first
+					if (counter == 'b0) begin
+						// first cycle, read in MSB
+						block_address[15:8] = prog_data;
+						prog_addr = prog_addr + 'b1;
+						counter = counter + 'b1;
+					end
+					else begin
+						// second cycle, read in LSB & continue to next state
+						block_address[7:0] = prog_data;
+						prog_addr = prog_addr + 'b1;
+						counter = 'b0;
+						state = PROG_DECODER_READ_TYPE;
+					end
+				end
+
+				PROG_DECODER_READ_TYPE: begin
+					// current value in ROM represents the block type
+					block_type = prog_data;
+					prog_addr = prog_addr + 'b1;
+					state = PROG_DECODER_READ_DATA;
+				end
+
+				PROG_DECODER_READ_DATA: begin
+					// read block_length bytes from ROM
+					if (counter < block_length) begin
+						// keep reading until done
+						block_data[counter] = prog_data;
+						prog_addr = prog_addr + 'b1;
+						counter = counter + 'b1;
+					end
+					else begin
+						// once block_length bytes have been read, block is
+						// finished
+						done = 'b1;
+						ready = 'b1;
+						state = PROG_DECODER_IDLE;
+					end
+				end
+			endcase
+		end
+	end
+
+endmodule
+/* lint_on */
---
+++ src/test/tb_program_decoder.sv
@@ -0,0 +1,118 @@
+// testbench for program_decoder module
+module tb_program_decoder();
+
+	parameter PROGRAM_FILE = "src/test/test_program.mem";
+	parameter PROGRAM_SIZE = 64;
+
+	// signals
+	logic clk, rst, start, ready, done;
+	logic [7:0] prog_data, block_length, block_type;
+	logic [dut.PROG_ADDR_BITS-1 : 0] prog_addr;
+	logic [15:0] block_address;
+	logic [7:0] block_data [dut.DATA_BLOCK_MAX_SIZE];
+
+	// program decoder on posedge
+	program_decoder #(.PROGRAM_SIZE(PROGRAM_SIZE)) dut (
+		clk, rst,
+		prog_addr, prog_data,
+		start, ready, done,
+		block_length, block_address, block_type, block_data
+	);
+
+	// ROM on negedge
+	rom #(
+		.FILE_NAME(PROGRAM_FILE),
+		.SIZE(PROGRAM_SIZE)
+	) rom_inst (~clk, prog_addr, prog_data);
+
+	// test
+	logic [7:0] expected_data [dut.PROGRAM_SIZE];
+	integer i, block;
+
+	initial begin
+		$dumpfile("trace/tb_program_decoder.vcd");
+		$dumpvars();
+
+		expected_data[0] = 'h48;
+		expected_data[1] = 'h65;
+		expected_data[2] = 'h6C;
+		expected_data[3] = 'h6C;
+		expected_data[4] = 'h6F;
+		expected_data[5] = 'h2C;
+		expected_data[6] = 'h20;
+		expected_data[7] = 'h77;
+		expected_data[8] = 'h6F;
+		expected_data[9] = 'h72;
+		expected_data[10] = 'h6C;
+		expected_data[11] = 'h64;
+		expected_data[12] = 'h21;
+		expected_data[13] = 'h00;
+		expected_data[14] = 'h00;
+		expected_data[15] = 'h00;
+
+		// reset
+		clk = 'b0;
+		rst = 'b1;
+
+		#10
+		clk = 'b1;
+
+		#10
+		clk = 'b0;
+		rst = 'b0;
+
+		#10 clk = 'b1;
+		#10 clk = 'b0;
+
+		// process first block of ROM
+		if (!ready) $error();
+		start = 'b1;
+
+		#10
+		clk = 'b1;
+
+		#10
+		clk = 'b0;
+		start = 'b0;
+		
+		while (!done) begin
+			#10 clk = 'b1;
+			#10 clk = 'b0;
+		end
+
+		// verify first block of ROM
+		if (block_length != 'd16) $error();
+		if (block_address != 'h1234) $error();
+		if (block_type != '0) $error();
+
+		for (i = 0; i < 16; i = i + 1) begin
+			if (block_data[i] != { 4'h1, i[3:0] }) $error();
+		end
+
+		// process first block of ROM
+		if (!ready) $error();
+		start = 'b1;
+
+		#10
+		clk = 'b1;
+
+		#10
+		clk = 'b0;
+		start = 'b0;
+		
+		while (!done) begin
+			#10 clk = 'b1;
+			#10 clk = 'b0;
+		end
+
+		// verify first block of ROM
+		if (block_length != 'd16) $error();
+		if (block_address != 'h5678) $error();
+		if (block_type != '0) $error();
+
+		for (i = 0; i < 16; i = i + 1) begin
+			if (block_data[i] != { 4'h2, i[3:0] }) $error();
+		end
+	end
+
+endmodule
