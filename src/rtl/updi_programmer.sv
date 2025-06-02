5f082e1ed10dda9604b0578cb8fdaa4594e7557f
Ryan Hausmann <ryanhausmann3@gmail.com>
2025-06-02 15:34:33 -0400

    Remove separate module for state machine for the updi_programmer module

--- src/rtl/updi_programmer.sv
+++ src/rtl/updi_programmer.sv
@@ -1,3 +1,16 @@
+// Possible states of the UPDI programmer.
+typedef enum {
+	UPDI_PROG_IDLE,
+	UPDI_PROG_RESET_UPDI,
+	UPDI_PROG_RESET_CHIP,
+	UPDI_PROG_READ_UPDI_STATUS,
+	UPDI_PROG_UNLOCK_CHIPERASE,
+	UPDI_PROG_UNLOCK_NVMPROG,
+	UPDI_PROG_READ_DEVICE_ID,
+	UPDI_PROG_PROGRAM_ROM,
+	UPDI_PROG_VERIFY_ROM
+} updi_programmer_state;
+
 // This module handles the high-level programming
 // of the UPDI-capable chip.
 module updi_programmer #(
@@ -19,13 +32,57 @@ module updi_programmer #(
 	output updi_bridge_mode updi_mode
 );
 	
-	// State machine instance
-	updi_programmer_sm state_machine (
-		.clk(clk),
-		.rst(rst),
-		.start(start),
-		.busy(busy),
-		.updi_mode(updi_mode)
-	);
+	// State machine
+	updi_programmer_state state;
+
+	always_ff @(posedge clk) begin
+		if (rst) begin
+			state = UPDI_PROG_IDLE;
+			updi_mode = UPDI_BRIDGE_MODE_IDLE;
+		end
+		else begin
+			case (state)
+				UPDI_PROG_IDLE: begin
+					busy = 'b0;
+
+					if (start) begin
+						state = UPDI_PROG_RESET_UPDI;
+					end
+				end
+				
+				UPDI_PROG_RESET_UPDI: begin
+				
+				end
+				
+				UPDI_PROG_RESET_CHIP: begin
+				
+				end
+				
+				UPDI_PROG_READ_UPDI_STATUS: begin
+				
+				end
+				
+				UPDI_PROG_UNLOCK_CHIPERASE: begin
+				
+				end
+				
+				UPDI_PROG_UNLOCK_NVMPROG: begin
+				
+				end
+				
+				UPDI_PROG_READ_DEVICE_ID: begin
+				
+				end
+				
+				UPDI_PROG_PROGRAM_ROM: begin
+				
+				end
+				
+				UPDI_PROG_VERIFY_ROM: begin
+				
+				end
+			endcase
+		end
+	end
 
 endmodule
