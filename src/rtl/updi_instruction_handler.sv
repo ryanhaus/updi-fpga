089705fdd39540f72e918e4f154c340d973ad1d1
Ryan Hausmann <ryanhausmann3@gmail.com>
2025-06-02 13:03:35 -0400

    Add instruction handler module & tb

---
+++ src/rtl/updi_instruction_handler.sv
@@ -0,0 +1,128 @@
+// All possible states for updi_instruction_handler
+typedef enum {
+	UPDI_INSTR_HDLR_IDLE,
+	UPDI_INSTR_HDLR_WR_SYNCH,
+	UPDI_INSTR_HDLR_WR_OPCODE,
+	UPDI_INSTR_HDLR_WR_DATA,
+	UPDI_INSTR_HDLR_WAIT_ACK
+} updi_instruction_handler_state;
+
+// Takes in instruction requests and writes out individual bytes representing
+// the instructions to an external FIFO. Also handles waiting for ACK signals
+module updi_instruction_handler #(
+	parameter MAX_DATA_SIZE = 16,
+	parameter DATA_ADDR_BITS = $clog2(MAX_DATA_SIZE)
+) (
+	input clk,
+	input rst,
+
+	// control signals
+	input start,
+	output logic ready,
+	output logic waiting_for_ack,
+	input ack_received,
+	
+	// instruction inputs
+	input [7:0] opcode, // see updi_instruction_converter
+	input [7:0] data [MAX_DATA_SIZE],
+	input [DATA_ADDR_BITS-1 : 0] data_len,
+	input [MAX_DATA_SIZE-1 : 0] wait_ack_after, // which bytes to wait for ACK after writing
+
+	// FIFO interface
+	output logic [7:0] fifo_data,
+	output logic fifo_wr_en,
+	input fifo_full
+);
+
+	// state machine
+	updi_instruction_handler_state state;
+	logic [DATA_ADDR_BITS-1 : 0] counter;
+
+	always_ff @(posedge clk) begin
+		fifo_wr_en = 'b0;
+		waiting_for_ack = 'b0;
+
+		if (rst) begin
+			state = UPDI_INSTR_HDLR_IDLE;
+			ready = 'b0;
+			fifo_data = 'b0;
+		end
+		else begin
+			case (state)
+				UPDI_INSTR_HDLR_IDLE: begin
+					// remain idle until start flag
+					ready = 'b1;
+
+					if (start) begin
+						ready = 'b0;
+						state = UPDI_INSTR_HDLR_WR_SYNCH;
+					end
+				end
+				
+				UPDI_INSTR_HDLR_WR_SYNCH: begin
+					// send SYNCH character (0x55)
+					if (!fifo_full) begin
+						fifo_data = 'h55;
+						fifo_wr_en = 'b1;
+						state = UPDI_INSTR_HDLR_WR_OPCODE;
+					end
+				end
+
+				UPDI_INSTR_HDLR_WR_OPCODE: begin
+					// push opcode to FIFO if possible
+					if (!fifo_full) begin
+						fifo_data = opcode;
+						fifo_wr_en = 'b1;
+
+						// if there is data, write that, otherwise idle
+						if (data_len > 'b0) begin
+							counter = 'b0;
+							state = UPDI_INSTR_HDLR_WR_DATA;
+						end
+						else begin
+							state = UPDI_INSTR_HDLR_IDLE;
+						end
+					end
+				end
+
+				UPDI_INSTR_HDLR_WR_DATA: begin
+					if (!fifo_full) begin
+						// continually push data to FIFO if possible
+						fifo_data = data[counter];
+						fifo_wr_en = 'b1;
+
+						// if done writing data, go back to idle, otherwise
+						// keep going
+						if (wait_ack_after[counter] == 'b1 && !ack_received) begin
+							state = UPDI_INSTR_HDLR_WAIT_ACK;
+						end
+						else if (counter == data_len - 'b1) begin
+							state = UPDI_INSTR_HDLR_IDLE;
+						end
+						else begin
+							counter = counter + 'b1;
+						end
+					end
+				end
+
+				UPDI_INSTR_HDLR_WAIT_ACK: begin
+					// wait for ack signal in between data bytes
+					waiting_for_ack = 'b1;
+
+					if (ack_received) begin
+						waiting_for_ack = 'b0;
+							
+						if (counter == data_len - 'b1) begin
+							state = UPDI_INSTR_HDLR_IDLE;
+						end
+						else begin
+							counter = counter + 'b1;
+							state = UPDI_INSTR_HDLR_WR_DATA;
+						end
+					end
+				end
+			endcase
+		end
+	end
+
+endmodule
---
+++ src/test/tb_updi_instruction_handler.sv
@@ -0,0 +1,212 @@
+// testbench for updi_instruction_handler
+module tb_updi_instruction_handler();
+
+	parameter MAX_DATA_SIZE = 16;
+	parameter DATA_ADDR_BITS = $clog2(MAX_DATA_SIZE);
+
+	// signals
+	logic clk, rst, start, ready, waiting_for_ack, ack_received, fifo_wr_en, fifo_full;
+	logic [7:0] opcode, fifo_data;
+	logic [7:0] data [MAX_DATA_SIZE];
+	logic [DATA_ADDR_BITS-1 : 0] data_len;
+	logic [MAX_DATA_SIZE-1 : 0] wait_ack_after;
+	logic [7:0] fifo_out;
+	logic fifo_rd_en, fifo_empty;
+
+	// fifo on negedge
+	fifo #(.DEPTH(4)) fifo_inst (
+		~clk, rst,
+		fifo_data, fifo_out,
+		fifo_rd_en, fifo_wr_en,
+		fifo_empty, fifo_full
+	);
+
+	// dut on posedge
+	updi_instruction_handler #( 
+		.MAX_DATA_SIZE(MAX_DATA_SIZE),
+		.DATA_ADDR_BITS(DATA_ADDR_BITS)
+	) dut (
+		clk, rst,
+		start, ready, waiting_for_ack, ack_received,
+		opcode, data, data_len, wait_ack_after,
+		fifo_data, fifo_wr_en, fifo_full
+	);
+
+	integer i;
+
+	initial begin
+		$dumpfile("trace/tb_updi_instruction_handler.vcd");
+		$dumpvars();
+
+		// reset
+		clk = 'b0;
+		rst = 'b1;
+
+		#10
+		clk = 'b1;
+
+		#10
+		clk = 'b0;
+		rst = 'b0;
+
+		#10 clk = 'b1;
+		#10 clk = 'b0;
+
+		// send test instruction w/o data
+		if (!ready) $error();
+		opcode = 'b11100101; // instruction to get 16-byte SIB
+		data = '{default: 'b0};
+		data_len = 'b0;
+		wait_ack_after = 'b0;
+		start = 'b1;
+
+		#10 clk = 'b1;
+		#10 clk = 'b0;
+		start = 'b0;
+
+		while (!ready) begin
+			#10 clk = 'b1;
+			#10 clk = 'b0;
+		end
+
+		// verify only SYNCH and the opcode was written to the FIFO
+		#10 clk = 'b1;
+
+		i = 0;
+		fifo_rd_en = 'b1;
+
+		while (!fifo_empty) begin
+			#10 clk = 'b0;
+			#10 clk = 'b1;
+
+			case (i)
+				0: if (fifo_out != 'h55) $error();
+				1: if (fifo_out != 'b11100101) $error();
+				default: $error();
+			endcase
+
+			i = i + 1;
+		end
+
+		fifo_rd_en = 'b0;
+		#10 clk = 'b0;
+
+		// write a command with some data that will fill the FIFO, make sure
+		// it stops properly & continues after to keep pushing data, as well
+		// as ensure it waits for ACK when necessary
+		if (!ready) $error();
+
+		opcode = 'b01000101;
+
+		data = '{default: 'b0};
+		data[0] = 'h12;
+		data[1] = 'h34;
+		data[2] = 'h56;
+		data[3] = 'h78;
+
+		data_len = 'd4;
+
+		wait_ack_after = 'b0;
+		wait_ack_after[1] = 'b1;
+		wait_ack_after[3] = 'b1;
+		
+		start = 'b1;
+
+		#10 clk = 'b1;
+		#10 clk = 'b0;
+
+		start = 'b0;
+
+		// make sure it waits while FIFO is full
+		for (i = 0; i < 10; i = i + 1) begin
+			#10 clk = 'b1;
+			#10 clk = 'b0;
+		end
+
+		// flush FIFO and verify data
+		#10 clk = 'b1;
+
+		i = 0;
+		fifo_rd_en = 'b1;
+
+		while (!fifo_empty) begin
+			#10 clk = 'b0;
+			#10 clk = 'b1;
+
+			case (i)
+				0: if (fifo_out != 'h55) $error();
+				1: if (fifo_out != 'h45) $error();
+				2: if (fifo_out != 'h12) $error();
+				3: if (fifo_out != 'h34) $error(); // this byte gets written as FIFO is read from
+				default: $error();
+			endcase
+
+			i = i + 1;
+		end
+
+		fifo_rd_en = 'b0;
+		#10 clk = 'b0;
+
+		// make sure ACK is waited for
+		for (i = 0; i < 5; i = i + 1) begin
+			#10 clk = 'b1;
+			#10 clk = 'b0;
+		end
+
+		do begin
+			#10 clk = 'b1;
+			#10 clk = 'b0;
+		end while (!waiting_for_ack);
+
+		for (i = 0; i < 5; i = i + 1) begin
+			#10 clk = 'b1;
+			#10 clk = 'b0;
+		end
+
+		ack_received = 'b1;
+		#10 clk = 'b1;
+		#10 clk = 'b0;
+		ack_received = 'b0;
+
+		// allow rest of writes to occur
+		for (i = 0; i < 10; i = i + 1) begin
+			#10 clk = 'b1;
+			#10 clk = 'b0;
+		end
+		
+		// flush FIFO and verify data
+		#10 clk = 'b1;
+
+		i = 0;
+		fifo_rd_en = 'b1;
+
+		while (!fifo_empty) begin
+			#10 clk = 'b0;
+			#10 clk = 'b1;
+
+			case (i)
+				0: if (fifo_out != 'h56) $error();
+				1: if (fifo_out != 'h78) $error();
+				default: $error();
+			endcase
+
+			i = i + 1;
+		end
+
+		fifo_rd_en = 'b0;
+		#10 clk = 'b0;
+
+		// make sure it is still waiting for last ACK
+		if (ready) $error();
+
+		ack_received = 'b1;
+		#10 clk = 'b1;
+		#10 clk = 'b0;
+		ack_received = 'b0;
+
+		if (!ready) $error();
+
+		$finish;
+	end
+
+endmodule
