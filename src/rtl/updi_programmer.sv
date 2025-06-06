1f08100a26af82e01e521e166cc6a5f23c4de354
Ryan Hausmann <ryanhausmann3@gmail.com>
2025-06-06 15:38:08 -0400

    Add first bits of the updi_programmer state machine

--- src/rtl/updi_programmer.sv
+++ src/rtl/updi_programmer.sv
@@ -168,19 +168,36 @@ module updi_programmer #(
 	
 	// State machine
 	updi_programmer_state state;
+	logic [7:0] counter;
 
 	always_ff @(posedge clk) begin
 		if (rst) begin
 			state <= UPDI_PROG_IDLE;
+			counter <= 'b0;
 		end
 		else begin
 			case (state)
 				UPDI_PROG_IDLE: begin
-
+					// wait for start signal
+					if (start) begin
+						counter <= 'b0;
+						state <= UPDI_PROG_RESET_UPDI;
+					end
 				end
 				
 				UPDI_PROG_RESET_UPDI: begin
-				
+					// wait until double break is done
+					if (counter == 'b0) begin
+						double_break_start <= 'b1;
+						counter <= 'b1;
+					end
+					else begin
+						double_break_start <= 'b0;
+
+						if (double_break_done) begin
+							state <= UPDI_PROG_RESET_CHIP;
+						end
+					end
 				end
 				
 				UPDI_PROG_RESET_CHIP: begin
@@ -215,13 +232,14 @@ module updi_programmer #(
 	end
 
 	always_comb begin
+		busy = 'b1;
+		
 		case (state)
 			UPDI_PROG_IDLE: begin
-
+				busy = 'b0;
 			end
 			
 			UPDI_PROG_RESET_UPDI: begin
-			
 			end
 			
 			UPDI_PROG_RESET_CHIP: begin
