49caef249a2d94453a32af8ee7041d8fbcca04bd
Ryan Hausmann <ryanhausmann3@gmail.com>
2025-06-10 11:12:15 -0400

    Add delay module and testbench

---
+++ src/rtl/delay.sv
@@ -0,0 +1,49 @@
+// waits a certain number of clock cycles after a start pulse to output a done
+// pulse
+module delay #(
+	parameter N_CLKS = 100
+) (
+	input clk,
+	input rst,
+
+	input start,
+	output logic done
+);
+
+	localparam COUNTER_BITS = $clog2(N_CLKS);
+	logic active;
+	logic [COUNTER_BITS-1 : 0] counter;
+
+	always_ff @(posedge clk) begin
+		if (rst) begin
+			counter <= 'b0;
+			active <= 'b0;
+		end
+		else begin
+			if (start) begin
+				active <= 'b1;
+			end
+
+			if (active) begin
+				if (counter == N_CLKS-1) begin
+					active <= 'b0;
+					counter <= 'b0;
+				end
+				else begin
+					counter <= counter + 'b1;
+				end
+			end
+		end
+	end
+
+	always_comb begin
+		done = 'b0;
+
+		if (active) begin
+			if (counter == N_CLKS-1) begin
+				done = 'b1;
+			end
+		end
+	end
+
+endmodule
---
+++ src/test/tb_delay.sv
@@ -0,0 +1,43 @@
+// testbench for delay module
+module tb_delay();
+
+	logic clk, rst, start, done;
+	delay #(.N_CLKS(100)) dut (clk, rst, start, done);
+
+	integer i;
+
+	initial #10000 $error();
+
+	initial begin
+		$dumpfile("trace/tb_delay.fst");
+		$dumpvars();
+
+		// reset
+		clk = 'b0;
+		rst = 'b1;
+		#10 clk = 'b1;
+		#10 clk = 'b0;
+		rst = 'b0;
+
+		// start a delay
+		start = 'b1;
+
+		#10 clk = 'b1;
+		#10 clk = 'b0;
+
+		start = 'b0;
+
+		// count clocks
+		i = 1; // including clock cycle for start signal
+		while (!done) begin
+			#10 clk = 'b1;
+			#10 clk = 'b0;
+			i = i + 1;
+		end
+
+		if (i != 100) $error();
+
+		$finish;
+	end
+
+endmodule
