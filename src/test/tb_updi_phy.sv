ee3d10e80f72b2800e2c0d7826f52c8f708d0a5b
Ryan Hausmann <ryanhausmann3@gmail.com>
2025-07-03 11:17:58 -0400

    Add second updi_phy instance to tb_updi_phy

--- src/test/tb_updi_phy.sv
+++ src/test/tb_updi_phy.sv
@@ -1,21 +1,39 @@
 module tb_updi_phy();
 
-	logic clk, rst, updi;
+	logic clk, rst;
 
-	// UPDI PHY instance
-	logic [7:0] uart_tx_fifo_data, uart_rx_fifo_data;
-	logic uart_tx_fifo_wr_en, uart_tx_fifo_full, uart_tx_fifo_almost_full,
-		uart_rx_fifo_rd_en, uart_rx_fifo_empty, uart_rx_fifo_almost_empty, rx_error,
-		double_break_start, double_break_busy, double_break_done;
+	// UPDI PHY instance 1
+	logic [7:0] uart_tx_fifo_data1, uart_rx_fifo_data1;
+	logic uart_tx_fifo_wr_en1, uart_tx_fifo_full1, uart_tx_fifo_almost_full1,
+		uart_rx_fifo_rd_en1, uart_rx_fifo_empty1, uart_rx_fifo_almost_empty1, rx_error1,
+		double_break_start1, double_break_busy1, double_break_done1,
+		updi1;
 
 	updi_phy #(
 		.DOUBLE_BREAK_PULSE_CLK(250)
-	) dut (
+	) dut1 (
 		clk, rst,
-		uart_tx_fifo_data, uart_tx_fifo_wr_en, uart_tx_fifo_full, uart_tx_fifo_almost_full,
-		uart_rx_fifo_data, uart_rx_fifo_rd_en, uart_rx_fifo_empty, uart_rx_fifo_almost_empty, rx_error,
-		double_break_start, double_break_busy, double_break_done,
-		updi
+		uart_tx_fifo_data1, uart_tx_fifo_wr_en1, uart_tx_fifo_full1, uart_tx_fifo_almost_full1,
+		uart_rx_fifo_data1, uart_rx_fifo_rd_en1, uart_rx_fifo_empty1, uart_rx_fifo_almost_empty1, rx_error1,
+		double_break_start1, double_break_busy1, double_break_done1,
+		updi1
+	);
+
+	// UPDI PHY instance 2
+	logic [7:0] uart_tx_fifo_data2, uart_rx_fifo_data2;
+	logic uart_tx_fifo_wr_en2, uart_tx_fifo_full2, uart_tx_fifo_almost_full2,
+		uart_rx_fifo_rd_en2, uart_rx_fifo_empty2, uart_rx_fifo_almost_empty2, rx_error2,
+		double_break_start2, double_break_busy2, double_break_done2,
+		updi2;
+
+	updi_phy #(
+		.DOUBLE_BREAK_PULSE_CLK(250)
+	) dut2 (
+		clk, rst,
+		uart_tx_fifo_data2, uart_tx_fifo_wr_en2, uart_tx_fifo_full2, uart_tx_fifo_almost_full2,
+		uart_rx_fifo_data2, uart_rx_fifo_rd_en2, uart_rx_fifo_empty2, uart_rx_fifo_almost_empty2, rx_error2,
+		double_break_start2, double_break_busy2, double_break_done2,
+		updi2
 	);
 
 	initial #100000 $error();
@@ -34,13 +52,13 @@ module tb_updi_phy();
 		rst = 'b0;
 
 		// test double break
-		double_break_start = 'b1;
+		double_break_start1 = 'b1;
 		#10 clk = 'b1;
 		#10 clk = 'b0;
-		double_break_start = 'b0;
+		double_break_start1 = 'b0;
 
 		i = 0;
-		while (!double_break_done) begin
+		while (!double_break_done1) begin
 			#10 clk = 'b1;
 			#10 clk = 'b0;
 			i = i + 1;
@@ -49,16 +67,16 @@ module tb_updi_phy();
 		if (i != 1000) $error();
 
 		// send test value
-		uart_tx_fifo_data = 'h55;
-		uart_tx_fifo_wr_en = 'b1;
+		uart_tx_fifo_data1 = 'h55;
+		uart_tx_fifo_wr_en1 = 'b1;
 		#10 clk = 'b1;
 		#10 clk = 'b0;
-		uart_tx_fifo_wr_en = 'b0;
+		uart_tx_fifo_wr_en1 = 'b0;
 
 		#10 clk = 'b1;
 		#10 clk = 'b0;
 
-		while (dut.uart_tx_active) begin
+		while (dut1.uart_tx_active) begin
 			#10 clk = 'b1;
 			#10 clk = 'b0;
 		end
